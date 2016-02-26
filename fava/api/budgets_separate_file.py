5ea392d2645825832c3fe9169d965580240af469
Dominik Aumayr <dominik@aumayr.name>
2016-02-26 13:43:05 +0100

    Support days, weeks, months, quarters and years
    ref #165

--- fava/api/budgets_separate_file.py
+++ fava/api/budgets_separate_file.py
@@ -11,6 +11,7 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
+
 class Budgets(object):
 
     def __init__(self, accounts):
@@ -47,7 +48,7 @@ class Budgets(object):
         budget = 0
         for single_day in daterange(date_from, date_to):
             matching_dateline = self._matching_dateline(datelines, single_day)
-            budget = budget + (matching_dateline.value/7)
+            budget = budget + (matching_dateline.value / matching_dateline.days)
 
         return budget
 
@@ -64,11 +65,31 @@ class Budgets(object):
 
 class Dateline(object):
 
-    def __init__(self, date_monday, value, currency):
+    def __init__(self, date_monday, period, value, currency):
         super(Dateline, self).__init__()
         self.date_monday = date_monday
+        self.period = period
         self.value = value
         self.currency = currency
+
+        self.days = self._days(self.date_monday, self.period)
+
+    def _days(self, start_date, period):
+        # TODO test this extensively, may be buggy
+        if period == 'D':
+            return 1
+        if period == 'W':
+            return 7
+        if period == 'M':
+            return (start_date.replace(month = start_date.month % 12 +1, day = 1) - timedelta(days=1)).day
+        if period == 'Q':
+            month1 = days_in_period(datetime(start_date.year, start_date.month, 1), 'M')
+            month2 = days_in_period(datetime(start_date.year, start_date.month+1, 1), 'M')
+            month3 = days_in_period(datetime(start_date.year, start_date.month+2, 1), 'M')
+            return ((start_date + timedelta(days=(month1+month2+month3))) - start_date).days
+        if period == 'Y':
+            return ((start_date + timedelta(years=1)) - start_date).days
+        raise Exception("Period unknown: {}".format(period))
 
 
 class AccountEntry(object):
@@ -82,11 +103,39 @@ class AccountEntry(object):
 
 grammar_whitespace_mode = 'optional'
 
-class DateExpr(Grammar):  # 2016-W01
-    grammar = (WORD('0-9'), WORD('-'), L('W'), WORD('0-9'))
+class DateExpr(Grammar):
+    """
+    A date can represented in these ways:
+        2016-01-01 => Budget for a day
+        2016-W01   => Budget for a week
+        2016-01    => Budget for a month
+        2016-Q1    => Budget for a quarter
+        2016       => Budget for a year
+
+    Returns a tuple of (date, 'D|W|M|Q|Y') where 'D|W|M|Q|Y' is the period
+    """
+    grammar = (WORD('0-9'), OR(GRAMMAR((WORD('-'), WORD('0-9'), WORD('-'), WORD('0-9'))) | GRAMMAR(WORD('-'), L('W'), WORD('0-9')) | GRAMMAR(WORD('-'), L('Q'), WORD('0-9'))))
 
     def value(self):
-        return datetime.strptime(self.string + '-1', "%Y-W%U-%w").date() # + timedelta(7)
+        if self.string.count('-') == 2:     # 2016-01-01
+            return ('D', datetime.strptime(self.string, "%Y-%m-%d").date())
+        elif 'W' in self.string:   # 2016-W01
+            return ('W', datetime.strptime(self.string + '-1', "%Y-W%U-%w").date())
+        elif 'Q' in self.string:   # 2016-Q1
+            if 'Q1' in self.string:
+                return ('Q', datetime.strptime(self.string + '-01-01', "%Y-Q1-%m-%d").date())
+            elif 'Q2' in self.string:
+                return ('Q', datetime.strptime(self.string + '-04-01', "%Y-Q2-%m-%d").date())
+            elif 'Q3' in self.string:
+                return ('Q', datetime.strptime(self.string + '-07-01', "%Y-Q3-%m-%d").date())
+            elif 'Q4' in self.string:
+                return ('Q', datetime.strptime(self.string + '-10-01', "%Y-Q4-%m-%d").date())
+            else:
+                raise Exception("Invalid date-string: {}".format(self.string))
+        elif '-' in self.string:   # 2016-01
+            return ('M', datetime.strptime(self.string + '-01', "%Y-%m-%d").date())
+        else:                      # 2016
+            return ('Y', datetime.strptime(self.string + '-01-01', "%Y-%m-%d").date())
 
 class ValueExpr(Grammar):
     grammar = (OPTIONAL(WORD('-')), WORD('0-9'), WORD('.'), WORD('0-9'))
@@ -112,7 +161,7 @@ class DatelineExpr(Grammar):
     def value(self):
         value = []
         for e in self[0]:
-            value.append(Dateline(date_monday=e[0].value(), value=e[1].value(), currency=e[2].value()))
+            value.append(Dateline(date_monday=e[0].value()[1], period=e[0].value()[0], value=e[1].value(), currency=e[2].value()))
         return value
 
 class EntryExpr(Grammar):
